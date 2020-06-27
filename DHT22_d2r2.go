585d5150589f2382cfca20ceb2c79cfb643bd420
gondril <alfred.gondril@gmail.com>
2020-06-27 10:55:53 +0700

    Update DHT22_d2r2.go

--- DHT22_d2r2.go
+++ DHT22_d2r2.go
@@ -6,10 +6,18 @@ import(
 )
 
 func main() {	
+	//di sini menggunakan GPIO 4
 	temperature, humidity, retried, err := dht.ReadDHTxxWithRetry(dht.DHT22, 4, false, 10)//jika ingin mengganti jadi dht11 ganti jadi dht.DHT11
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Printf("Temperature = %v*C, Humidity = %v%% (retried %d times)\n",temperature, humidity, retried)
+	log.Printf("Temperature = %v*C, Humidity = %v%% (retried %d times)\n\n",temperature, humidity, retried)
+	for {
+		temperature, humidity, retried, err = dht.ReadDHTxxWithRetry(dht.DHT22, 4, false, 10)//jika ingin mengganti jadi dht11 ganti jadi dht.DHT11
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("Temperature = %v*C, Humidity = %v%% (retried %d times)\n\n",temperature, humidity, retried)	
+	}
 }
 
